0a5ced93620e5831cddaefe4b022559cf87998d7
Grace Li <fullofgrace88@gmail.com>
2012-09-05 18:45:27 -0400

    [#35485935] Uses chapterbib package to get individual references for each chapter.  Still have to figure out how to customize the Bibliography page title to make it a little smaller. For now, don't spend anytime on this until the important stuff is done.

---
+++ document/makepdf.sh
@@ -0,0 +1,5 @@
+pdflatex -interaction=nonstopmode my-thesis.tex
+bibtex my-thesis.aux
+pdflatex -interaction=nonstopmode my-thesis.tex
+pdflatex -interaction=nonstopmode my-thesis.tex
+open my-thesis.pdf
