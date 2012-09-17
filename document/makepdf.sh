2f58072b28be1b21451a5fa8ef2ad665d43a3525
Grace Li <fullofgrace88@gmail.com>
2012-09-17 18:57:51 -0400

    [#35856233,#35417775] Added relevant figures and equations. Minor improvements on organization. Added some details.

--- document/makepdf.sh
+++ document/makepdf.sh
@@ -1,5 +1,5 @@
 pdflatex -interaction=nonstopmode my-thesis.tex
-bibtex introudction.aux
+bibtex introduction.aux
 bibtex introduction-MD-methods.aux
 pdflatex -interaction=nonstopmode my-thesis.tex
 pdflatex -interaction=nonstopmode my-thesis.tex
