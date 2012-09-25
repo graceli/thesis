28969fb26e557751b88970c316f37b7defe113d5
Grace Li <grace.li@utoronto.ca>
2012-09-25 15:02:17 -0400

    [#35650059] Fixed references in results1 to bibtex citation keys. References now appear in bibliography. Added more symbols. Added more new command shortcuts. Added a inline table (but its broken).

--- document/makepdf.sh
+++ document/makepdf.sh
@@ -1,7 +1,7 @@
 pdflatex -interaction=nonstopmode my-thesis.tex
 bibtex introduction.aux
 bibtex introduction-MD-methods.aux
-
+bibtex results1/results1.aux
 # make nomenclature
 makeindex my-thesis.nlo -s nomencl.ist -o my-thesis.nls
 
