83bf6eae215dff861b49acf5e15c18a94b3072fe
Grace Li <grace.li@utoronto.ca>
2013-09-26 22:58:46 -0400

    [starts #35417757] Added the results2 (KLVFFAE) paper into the thesis. Still needs a lot of work to get it into shape for the thesis.

--- document/makepdf_full.sh
+++ document/makepdf_full.sh
@@ -4,8 +4,8 @@ bibtex introduction.aux
 bibtex methods.aux
 bibtex results1/results1.aux
 bibtex results2/results2.aux
-bibtex results3/results2.aux
-bibtex results4/results2.aux
+bibtex results3/results3.aux
+bibtex results4/results4.aux
 bibtex conclusions/conclusions.aux
 
 # make nomenclature
